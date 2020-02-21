72e2d8d5ab6e9e9d8fb5c16e1ce6bde2c8ce31e2
小久保洋平 <yheihei0126@gmail.com>
2020-02-21 13:21:06 +0900

    header.php登録 トップページを構築する #1

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); //html要素のlang属性を出力 ?>">
+
+<head profile="http://gmpg.org/xfn/11">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>" />
+<meta http-equiv="content-style-type" content="text/css" />
+<meta http-equiv="content-script-type" content="text/javascript" />
+<meta name="author" content="" />
+<meta name="keyword" content="" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/common.js"></script>
+<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo('name'); ?></title>
+<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+<!-- <meta name="description" content="何も考えずに銀行や不動産会社の言うままに住宅ローンを組むと非常に大きな損失が生じます！最も賢く選んで頂くために金利をはじめ、各銀行の住宅ローンについて調査しています。" /> -->
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); //bodyタグにページの種類に応じたクラス名を与える ?>>
+
+<div id="base">
+  <div id="header">
+    <h1><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" width="394" height="43" alt="<?php wp_title( '|', true, 'right' ); ?><?php bloginfo('name'); ?>" /></a></h1>
+  </div>
+  <!--/header end-->
--- index.php
+++ index.php
@@ -1,35 +1,6 @@
 
-<!DOCTYPE html>
-<html lang="ja">
+<?php get_header(); ?>
 
-<head profile="http://gmpg.org/xfn/11">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-<meta http-equiv="content-style-type" content="text/css" />
-<meta http-equiv="content-script-type" content="text/javascript" />
-<meta name="author" content="" />
-<meta name="keyword" content="" />
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/common.js"></script>
-<!-- InstanceBeginEditable name="doctitle" -->
-<title>住宅ローン金利の知恵｜減税効果と金利動向の2017年度 研究＆比較</title>
-<!-- InstanceEndEditable -->
-<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css" type="text/css" media="screen" />
-<!-- InstanceBeginEditable name="meta" -->
-<meta name="description" content="何も考えずに銀行や不動産会社の言うままに住宅ローンを組むと非常に大きな損失が生じます！最も賢く選んで頂くために金利をはじめ、各銀行の住宅ローンについて調査しています。" />
-<!-- InstanceEndEditable -->
-<!-- InstanceBeginEditable name="head" -->
-<!-- InstanceEndEditable -->
-<!-- InstanceParam name="logo" type="boolean" value="false" -->
-</head>
-
-<body>
-
-<div id="base">
-  <div id="header">
-    <h1><a href="./"><img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" width="394" height="43" alt="住宅ローン金利の知恵｜減税効果と金利動向の2017年度 研究＆比較" /></a></h1>
-  </div>
-  <!--/header end-->
 <div id="main">
 <div id="content">
 
