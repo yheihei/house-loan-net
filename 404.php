c477226690a087898f028b40bf7a1f60c4864b58
Yohei Kokubo <yhei.register@gmail.com>
2020-03-21 16:19:41 +0900

    検索結果一覧ページ構築 #11

---
+++ 404.php
@@ -0,0 +1,15 @@
+
+<?php get_header(); ?>
+
+<div id="main">
+<div id="content">
+
+  <h2>お探しのページが見つかりません。</h2>
+
+</div><!-- /content -->
+
+     <div id="side">
+     <?php get_sidebar(); //sidebar.phpを取得 ?>
+  </div><!--/side end-->
+
+<?php get_footer();
---
+++ search.php
@@ -0,0 +1,24 @@
+
+<?php get_header(); ?>
+
+<div id="main">
+<div id="content">
+  <section class="search-header">
+    <h1>検索キーワード:<?php echo get_search_query(); ?></h1>
+  </section>
+<?php if ( have_posts() ) : ?>
+  <?php get_template_part( 'templates/archives' ); ?>
+<?php else : // 条件分岐：投稿が無い場合は ?>
+
+  <h2>投稿が見つかりません。</h2>
+
+<?php endif; // 条件分岐終了 ?>
+
+
+</div><!-- /content -->
+
+     <div id="side">
+     <?php get_sidebar(); //sidebar.phpを取得 ?>
+  </div><!--/side end-->
+
+<?php get_footer();
